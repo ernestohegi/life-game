52877769e469246f3cbe69ca06effc964f2f4d1b
Ernesto Hegi <ernesto.hegi@gmail.com>
2016-03-14 20:26:34 +0000

    Cleans the life code.

--- src/es6/life.js
+++ src/es6/life.js
@@ -9,30 +9,25 @@ let Life = (function ($) {
           ALIVE_COLOR                 = '#000',
           DEAD_COLOR                  = '#FFF';
 
-    let requestAnimationFrameId = '';
+    let requestAnimationFrameId = '',
+        colors                  = false,
+        speed                   = 1000,
+        newRows                 = [],
+        rows                    = [],
+        gridSize                = {},
+        $container              = $(),
+        $rows                   = $(),
+        $survivor               = $('<span class="survivor"/>'),
+        item                    = '';
 
     return {
-        $container      : $(),
-        $survivor       : $('<span class="survivor"/>'),
-        $rows           : $(),
-        rows            : [],
-        newRows         : [],
-        caterpillars    : [],
-        speed           : 1000,
-        colors          : false,
-        evolver         : 0,
-        gridSize        : {},
-        total           : () => this.gridSize.x * this.gridSize.y ,
-        randomColor     : ALIVE_COLOR,
-        item            : '',
-        interval        : '',
         init: function (params) {
             if (params === undefined || params.container === undefined || params.item === undefined) {
                 throw 'No params specified.';
             }
 
-            this.$container = params.container;
-            this.item = params.item;
+            $container = params.container;
+            item = params.item;
 
             this.setGridSize(40, 40);
             this.createGrid();
@@ -40,8 +35,8 @@ let Life = (function ($) {
             this.updateSettings(params);
         },
         setGridSize: function (x, y) {
-            this.gridSize.x = x;
-            this.gridSize.y = y;
+            gridSize.x = x;
+            gridSize.y = y;
         },
         start: function () {
             requestAnimationFrameId = requestAnimationFrame(this.runGrid.bind(this));
@@ -52,62 +47,63 @@ let Life = (function ($) {
         createGrid: function () {
             let i, j;
 
-            this.rows = [];
-            this.newRows = [];
+            rows = [];
+            newRows = [];
 
-            for (i = 0; i < this.gridSize.x; ++i) {
-                this.rows.push([]);
-                this.newRows.push([]);
+            for (i = 0; i < gridSize.x; ++i) {
+                rows.push([]);
+                newRows.push([]);
 
-                for (j = 0; j < this.gridSize.y; ++j) {
-                    this.rows[i].push(DIE);
-                    this.newRows[i].push(DIE);
+                for (j = 0; j < gridSize.y; ++j) {
+                    rows[i].push(DIE);
+                    newRows[i].push(DIE);
                 }
             }
         },
         drawGrid: function () {
             var open, close,
-                item = '',
+                newItem = '',
                 survivor,
-                width;
+                width,
+                x;
 
-            for (var x = 0; x < this.gridSize.x; x++) {
-                open = '<' + this.item + ' data-row="' + x + '">';
-                item += open;
+            for (x = 0; x < gridSize.x; x++) {
+                open = '<' + item + ' data-row="' + x + '">';
+                newItem += open;
 
-                for (var y = 0; y < this.gridSize.y; y++) {
-                    survivor = this.$survivor.clone();
+                for (var y = 0; y < gridSize.y; y++) {
+                    survivor = $survivor.clone();
                     survivor.attr('data-column', y);
                     survivor.attr(DATA_STATUS_ATTRIBUTE, DIE);
 
-                    item += survivor.wrap('<div>').parent().html();
+                    newItem += survivor.wrap('<div>').parent().html();
                 }
 
-                close = '</' + this.item + '>';
-                item += close;
+                close = '</' + item + '>';
+                newItem += close;
             }
 
-            this.$container.html(item);
-            this.$rows = this.$container.find('li');
+            $container.html(newItem);
+            $rows = $container.find('li');
 
             // Get elements width after insertion to its parent
-            width = this.gridSize.x * this.$container.find('.survivor').width();
+            width = gridSize.x * $container.find('.survivor').width();
 
-            this.$container.width(width);
-            this.$container.parent('.container').width(width);
+            $container.width(width);
+            $container.parent('.container').width(width);
 
             this.createGrid();
         },
         runGrid: function () {
             let i, j;
 
-            for (i = 0; i < this.gridSize.x; ++i) {
-                for (j = 0; j < this.gridSize.y; ++j) {
+            for (i = 0; i < gridSize.x; ++i) {
+                for (j = 0; j < gridSize.y; ++j) {
                     this.checkNeighbors(i, j);
                 }
             }
 
-            this.updateRows(this.newRows);
+            this.updateRows(newRows);
             this.updateSurvivors();
 
             requestAnimationFrameId = requestAnimationFrame(this.runGrid.bind(this));
@@ -127,44 +123,40 @@ let Life = (function ($) {
                 }
             }
 
-            this.newRows[x][y] = this.getDestiny(neighborsCount, (this.newRows[x][y] === LIVE));
+            newRows[x][y] = this.getDestiny(neighborsCount, (newRows[x][y] === LIVE));
         },
         getDestiny: function (neighborsCount, isAlive) {
             return ((isAlive && (neighborsCount === 3 || neighborsCount === 2)) || (isAlive === false && neighborsCount === 3)) ? LIVE : DIE;
         },
         isInsideTheXAxis: function (index, x) {
-            return this.isInsideTheCoordinates(index, x, this.gridSize.x);
+            return this.isInsideTheCoordinates(index, x, gridSize.x);
         },
         isInsideTheYAxis: function (index, y) {
-            return this.isInsideTheCoordinates(index, y, this.gridSize.y);
+            return this.isInsideTheCoordinates(index, y, gridSize.y);
         },
         isInsideTheCoordinates: function (coordinate, position, gridCoordinate) {
             return coordinate !== undefined && position !== undefined && coordinate > -1 && coordinate <= gridCoordinate;
         },
         isNeighborAlive: function (x, y) {
-            return this.rows[x] !== undefined && this.rows[x][y] !== undefined && this.rows[x][y] === LIVE;
+            return rows[x] !== undefined && rows[x][y] !== undefined && rows[x][y] === LIVE;
         },
         updateRows: function (newRows) {
-            this.rows = newRows;
+            rows = newRows;
         },
-        /*
-        * Updates survivors status.
-        * Updates the grid with new states.
-        */
         updateSurvivors: function () {
-            var $rows = this.$rows.clone(),
+            var $newRows = $rows.clone(),
                 $survivor,
                 thisRowStatus,
                 backgroundColor = DEAD_COLOR;
 
-            for (var i = 0; i < this.gridSize.x; i++) {
-                for (var j = 0; j < this.gridSize.y; j++) {
-                    $survivor = $rows.filter('[data-row='+ i +']').find('[data-column='+ j +']');
-                    thisRowStatus = this.rows[i][j];
+            for (var i = 0; i < gridSize.x; i++) {
+                for (var j = 0; j < gridSize.y; j++) {
+                    $survivor = $newRows.filter('[data-row='+ i +']').find('[data-column='+ j +']');
+                    thisRowStatus = rows[i][j];
 
                     if ($survivor.attr(DATA_STATUS_ATTRIBUTE) !== thisRowStatus) {
                         if (thisRowStatus === LIVE) {
-                            backgroundColor = (this.colors) ? this.getRandomColor() : ALIVE_COLOR;
+                            backgroundColor = (colors) ? this.getRandomColor() : ALIVE_COLOR;
                         }
 
                         $survivor.toggleClass(SELECTED_CLASS_NAME);
@@ -174,33 +166,27 @@ let Life = (function ($) {
                 }
             }
 
-            this.$container.html('');
-            this.$container.append($rows);
+            $container.html('');
+            $container.append($newRows);
         },
-        /*
-        * Selects survivor
-        */
         selectSurvivor: function ($this) {
             var newStatus = $this.attr(DATA_STATUS_ATTRIBUTE) === DIE ? LIVE : DIE;
 
             $this.toggleClass(SELECTED_CLASS_NAME);
             $this.attr(DATA_STATUS_ATTRIBUTE, newStatus);
 
-            this.rows[$this.parent().data('row')][$this.data('column')] = newStatus;
+            rows[$this.parent().data('row')][$this.data('column')] = newStatus;
 
-            $this.css(BACKGROUND_COLOR_PROPERTY, (this.colors) ? this.getRandomColor() : ALIVE_COLOR);
+            $this.css(BACKGROUND_COLOR_PROPERTY, (colors) ? this.getRandomColor() : ALIVE_COLOR);
         },
-        /*
-        * Updates current settings
-        */
         updateSettings: function (params) {
             if (params !== undefined) {
                 if (params.speed !== undefined) {
-                    this.speed = params.speed;
+                    speed = params.speed;
                 }
 
                 if (params.colors !== undefined) {
-                    this.colors = params.colors;
+                    colors = params.colors;
                 }
             }
         },
