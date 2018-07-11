d34b8d6747ea9b1f23a1bb76243a6a6576fd22ce
Ernesto Hegi <ernesto.hegi@gmail.com>
2018-07-11 12:57:52 +0100

    Migrates rendering engine to canvas.

--- src/es6/interface.js
+++ src/es6/interface.js
@@ -1,83 +1,82 @@
-let Interface = (($) => {
-    'use strict';
+let Interface = ($ => {
+  "use strict";
 
-    const ID_TOKEN                  = '#',
-          CLASS_TOKEN               = '.',
-          CLICK_EVENT_NAME          = 'click',
-          MOUESOVER_EVENT_NAME      = 'mouseover',
-          REACT_CONTAINER_ID_NAME   = 'react',
-          SURVIVOR_SELECTOR         = CLASS_TOKEN + 'survivor',
-          MENU_SELECTOR             = ID_TOKEN + 'menu',
-          START_SELECTOR            = ID_TOKEN + 'start',
-          CLEAR_SELECTOR            = ID_TOKEN + 'clear',
-          COLORIZE_SELECTOR         = ID_TOKEN + 'colorize',
-          STOP_SELECTOR             = ID_TOKEN + 'stop',
-          CELLS_CONTAINERS_AMOUNT   = 10,
-          CELLS_AMOUNT              = 10;
+  const ID_TOKEN = "#",
+    CLASS_TOKEN = ".",
+    CLICK_EVENT_NAME = "click",
+    MOUESOVER_EVENT_NAME = "mouseover",
+    REACT_CONTAINER_ID_NAME = "react",
+    SURVIVOR_SELECTOR = CLASS_TOKEN + "survivor",
+    MENU_SELECTOR = ID_TOKEN + "menu",
+    START_SELECTOR = ID_TOKEN + "start",
+    CLEAR_SELECTOR = ID_TOKEN + "clear",
+    COLORIZE_SELECTOR = ID_TOKEN + "colorize",
+    STOP_SELECTOR = ID_TOKEN + "stop",
+    CELLS_CONTAINERS_AMOUNT = 10,
+    CELLS_AMOUNT = 10;
 
-    return {
-        colors: false,
-        init: function (params) {
-            this.$container = params.container;
-            this.$mainElement = $(MENU_SELECTOR);
-            this.drawOrganism();
-            this.bindEvents();
-        },
-        drawOrganism: () => {
-            ReactDOM.render(
-                React.createElement(
-                    Organism,
-                    {
-                        cellContainersAmount: CELLS_CONTAINERS_AMOUNT,
-                        cellsAmount: CELLS_AMOUNT
-                    }
-                ),
-                document.getElementById(REACT_CONTAINER_ID_NAME)
-            );
-        },
-        selectSurvivor:  survivorElement => {
-            Life.selectSurvivor(
-                $(survivorElement)
-            );
-        },
-        clearGrid: function () {
-            Life.stop();
-            Life.drawGrid();
-            Life.createGrid();
-        },
-        colorizeGrid: function () {
-            let colors = !this.colors;
+  return {
+    colors: false,
+    init: function(params) {
+      this.$container = params.container;
+      this.$mainElement = $(MENU_SELECTOR);
+      this.bindEvents();
+    },
+    selectSurvivor: survivorElement => {
+      Life.selectSurvivor($(survivorElement));
+    },
+    clearGrid: function() {
+      Life.stop();
+      Life.drawGrid();
+      Life.createGrid();
+    },
+    colorizeGrid: function() {
+      let colors = !this.colors;
 
-            Life.updateSettings({
-                colors
-            });
+      Life.updateSettings({
+        colors
+      });
 
-            this.colors = colors;
-        },
-        handleSurvivorClick: function (e) {
-            this.selectSurvivor(e.currentTarget);
-        },
-        handleSurvivorMouseover: function (e) {
-            if (e.ctrlKey || e.altKey) {
-                this.selectSurvivor(e.currentTarget);
-            }
-        },
-        /*
+      this.colors = colors;
+    },
+    handleSurvivorClick: function(e) {
+      this.selectSurvivor(e.currentTarget);
+    },
+    handleSurvivorMouseover: function(e) {
+      if (e.ctrlKey || e.altKey) {
+        this.selectSurvivor(e.currentTarget);
+      }
+    },
+    /*
         * Events binding
         */
-        bindEvents: function () {
-            this.$mainElement.on(CLICK_EVENT_NAME, START_SELECTOR, () => Life.start());
+    bindEvents: function() {
+      this.$mainElement.on(CLICK_EVENT_NAME, START_SELECTOR, () =>
+        Life.start()
+      );
 
-            this.$mainElement.on(CLICK_EVENT_NAME, STOP_SELECTOR, () => Life.stop());
+      this.$mainElement.on(CLICK_EVENT_NAME, STOP_SELECTOR, () => Life.stop());
 
-            this.$mainElement.on(CLICK_EVENT_NAME, CLEAR_SELECTOR, this.clearGrid);
+      this.$mainElement.on(CLICK_EVENT_NAME, CLEAR_SELECTOR, this.clearGrid);
 
-            this.$mainElement.on(CLICK_EVENT_NAME, COLORIZE_SELECTOR, this.colorizeGrid);
+      this.$mainElement.on(
+        CLICK_EVENT_NAME,
+        COLORIZE_SELECTOR,
+        this.colorizeGrid
+      );
 
-            // Draw seeds
-            this.$container.on(CLICK_EVENT_NAME, SURVIVOR_SELECTOR, this.handleSurvivorClick.bind(this));
+      // Draw seeds
+      this.$container.on(
+        CLICK_EVENT_NAME,
+        SURVIVOR_SELECTOR,
+        this.handleSurvivorClick.bind(this)
+      );
 
-            this.$container.on(MOUESOVER_EVENT_NAME, SURVIVOR_SELECTOR, this.handleSurvivorMouseover.bind(this));
-        }
-    };
+      this.$container.on(
+        MOUESOVER_EVENT_NAME,
+        SURVIVOR_SELECTOR,
+        this.handleSurvivorMouseover.bind(this)
+      );
+    }
+  };
 })(jQuery);
--- src/es6/life.js
+++ src/es6/life.js
@@ -1,212 +1,207 @@
-let Life = (function ($) {
-    'use strict';
+let Life = (function($) {
+  "use strict";
 
-    const BACKGROUND_COLOR_PROPERTY   = 'background-color',
-          DATA_STATUS_ATTRIBUTE       = 'data-status',
-          SELECTED_CLASS_NAME         = 'selected',
-          LIVE                        = 'live',
-          DIE                         = 'die',
-          ALIVE_COLOR                 = '#000',
-          DEAD_COLOR                  = '#FFF';
+  const BACKGROUND_COLOR_PROPERTY = "background-color",
+    DATA_STATUS_ATTRIBUTE = "data-status",
+    SELECTED_CLASS_NAME = "selected",
+    LIVE = "live",
+    DIE = "die",
+    ALIVE_COLOR = "#000",
+    DEAD_COLOR = "#FFF";
 
-    let requestAnimationFrameId = '',
-        colors                  = false,
-        speed                   = 1000,
-        newRows                 = [],
-        rows                    = [],
-        gridSize                = {},
-        $container              = $(),
-        $rows                   = $(),
-        $survivor               = $('<span class="survivor"/>'),
-        item                    = '';
+  let requestAnimationFrameId = "",
+    colors = false,
+    newRows = [],
+    rows = [],
+    gridSize = {},
+    $container = $(),
+    $rows = $(),
+    speed = 0,
+    canvasContext = {};
 
-    return {
-        init: function (params) {
-            if (params === undefined || params.container === undefined || params.item === undefined) {
-                throw 'No params specified.';
-            }
+  return {
+    init: function(params) {
+      if (params === undefined || params.container === undefined) {
+        throw "No params specified.";
+      }
 
-            $container = params.container;
-            item = params.item;
+      $container = params.container;
 
-            this.setGridSize(40, 40);
-            this.createGrid();
-            this.drawGrid();
-            this.updateSettings(params);
-        },
-        setGridSize: function (x, y) {
-            gridSize.x = x;
-            gridSize.y = y;
-        },
-        start: function () {
-            requestAnimationFrameId = requestAnimationFrame(this.runGrid.bind(this));
-        },
-        stop: function () {
-             cancelAnimationFrame(requestAnimationFrameId);
-        },
-        createGrid: function () {
-            let i, j;
+      this.setUpGrid(40, 40, 20);
+      this.createGrid();
+      this.drawGrid();
+      this.updateSettings(params);
+    },
+    setUpGrid: function(x, y, z) {
+      const canvas = document.getElementById("game");
 
-            rows = [];
-            newRows = [];
+      gridSize.x = x;
+      gridSize.y = y;
+      gridSize.z = z;
 
-            for (i = 0; i < gridSize.x; ++i) {
-                rows.push([]);
-                newRows.push([]);
+      canvasContext = canvas.getContext("2d");
+    },
+    start: function() {
+      requestAnimationFrameId = requestAnimationFrame(this.runGrid.bind(this));
+    },
+    stop: function() {
+      cancelAnimationFrame(requestAnimationFrameId);
+    },
+    createGrid: function() {
+      let i, j;
 
-                for (j = 0; j < gridSize.y; ++j) {
-                    rows[i].push(DIE);
-                    newRows[i].push(DIE);
-                }
-            }
-        },
-        drawGrid: function () {
-            var open, close,
-                newItem = '',
-                survivor,
-                width,
-                x;
+      rows = [];
+      newRows = [];
 
-            for (x = 0; x < gridSize.x; x++) {
-                open = '<' + item + ' data-row="' + x + '">';
-                newItem += open;
+      for (i = 0; i < gridSize.x; ++i) {
+        rows.push([]);
+        newRows.push([]);
 
-                for (var y = 0; y < gridSize.y; y++) {
-                    survivor = $survivor.clone();
-                    survivor.attr('data-column', y);
-                    survivor.attr(DATA_STATUS_ATTRIBUTE, DIE);
-
-                    newItem += survivor.wrap('<div>').parent().html();
-                }
-
-                close = '</' + item + '>';
-                newItem += close;
-            }
-
-            $container.html(newItem);
-            $rows = $container.find('li');
-
-            // Get elements width after insertion to its parent
-            width = gridSize.x * $container.find('.survivor').width();
-
-            $container.width(width);
-            $container.parent('.container').width(width);
-
-            this.createGrid();
-        },
-        runGrid: function () {
-            let i, j;
-
-            for (i = 0; i < gridSize.x; ++i) {
-                for (j = 0; j < gridSize.y; ++j) {
-                    this.checkNeighbors(i, j);
-                }
-            }
-
-            this.updateRows(newRows);
-            this.updateSurvivors();
-
-            requestAnimationFrameId = requestAnimationFrame(this.runGrid.bind(this));
-        },
-        checkNeighbors: function (x, y) {
-            let neighborsCount = 0,
-                i,
-                j;
-
-            for(i = x-1; i <= x+1; ++i) {
-                if (this.isInsideTheXAxis(i, x)) {
-                    for(j = y-1; j <= y+1; ++j) {
-                        if (this.isNotItself(x, y, i, j) && this.isInsideTheYAxis(j, y) && this.isNeighborAlive(i, j)) {
-                            ++neighborsCount;
-                        }
-                    }
-                }
-            }
-
-            newRows[x][y] = this.getDestiny(neighborsCount, (newRows[x][y] === LIVE));
-
-            return newRows;
-        },
-        getDestiny: function (neighborsCount, isAlive) {
-            return ((isAlive && (neighborsCount === 3 || neighborsCount === 2)) || (isAlive === false && neighborsCount === 3)) ? LIVE : DIE;
-        },
-        isNotItself: function (x, y, i, j) {
-            return (x === i && y === j) === false;
-        },
-        isInsideTheXAxis: function (index, x) {
-            return this.isInsideTheCoordinates(index, x, gridSize.x);
-        },
-        isInsideTheYAxis: function (index, y) {
-            return this.isInsideTheCoordinates(index, y, gridSize.y);
-        },
-        isInsideTheCoordinates: function (coordinate, position, gridCoordinate) {
-            return coordinate !== undefined && position !== undefined && coordinate > -1 && coordinate <= gridCoordinate;
-        },
-        isNeighborAlive: function (x, y) {
-            return newRows[x] !== undefined && newRows[x][y] !== undefined && newRows[x][y] === LIVE;
-        },
-        updateRows: function (newRows) {
-            rows = newRows;
-        },
-        updateSurvivors: function () {
-            $container.html('').append(this.checkSurvivors());
-        },
-        checkSurvivors: function () {
-            let $newRows = $rows.clone(),
-                $survivor,
-                thisRowStatus;
-
-            for (var i = 0; i < gridSize.x; i++) {
-                for (var j = 0; j < gridSize.y; j++) {
-                    $survivor = $newRows.filter('[data-row='+ i +']').find('[data-column='+ j +']');
-                    thisRowStatus = rows[i][j];
-
-                    this.updateSurvivor($survivor, thisRowStatus);
-                }
-            }
-
-            return $newRows;
-        },
-        updateSurvivor: function ($survivor, thisRowStatus) {
-            let backgroundColor = DEAD_COLOR;
-
-            if ($survivor.attr(DATA_STATUS_ATTRIBUTE) !== thisRowStatus) {
-                if (thisRowStatus === LIVE) {
-                    backgroundColor = (colors) ? this.getRandomColor() : ALIVE_COLOR;
-                }
-
-                $survivor.toggleClass(SELECTED_CLASS_NAME);
-                $survivor.css(BACKGROUND_COLOR_PROPERTY, backgroundColor);
-                $survivor.attr(DATA_STATUS_ATTRIBUTE, thisRowStatus);
-            }
-        },
-        selectSurvivor: function ($this) {
-            var newStatus = $this.attr(DATA_STATUS_ATTRIBUTE) === DIE ? LIVE : DIE;
-
-            this.setRowStatus($this.parent().data('row'), $this.data('column'), newStatus);
-
-            $this.toggleClass(SELECTED_CLASS_NAME);
-            $this.attr(DATA_STATUS_ATTRIBUTE, newStatus);
-            $this.css(BACKGROUND_COLOR_PROPERTY, (colors) ? this.getRandomColor() : ALIVE_COLOR);
-        },
-        setRowStatus: function (x, y, status) {
-            newRows[x][y] = status;
-
-            return newRows;
-        },
-        updateSettings: function (params) {
-            if (params !== undefined) {
-                if (params.speed !== undefined) {
-                    speed = params.speed;
-                }
-
-                if (params.colors !== undefined) {
-                    colors = params.colors;
-                }
-            }
-        },
-        getRandomColor: function () {
-            return '#'+Math.floor(Math.random()*16777215).toString(16);
+        for (j = 0; j < gridSize.y; ++j) {
+          rows[i].push(DIE);
+          newRows[i].push(DIE);
         }
-    };
+      }
+    },
+    drawGrid: function() {
+      for (let x = 0; x < gridSize.x; x++) {
+        for (let y = 0; y < gridSize.y; y++) {
+          canvasContext.fillStyle = DEAD_COLOR;
+
+          canvasContext.fillRect(
+            x * gridSize.z,
+            y * gridSize.z,
+            gridSize.z,
+            gridSize.z
+          );
+        }
+      }
+    },
+    runGrid: function() {
+      for (let i = 0; i < gridSize.x; ++i) {
+        for (let j = 0; j < gridSize.y; ++j) {
+          this.checkNeighbors(i, j);
+        }
+      }
+
+      this.updateSurvivors(newRows);
+
+      requestAnimationFrameId = requestAnimationFrame(this.runGrid.bind(this));
+    },
+    checkNeighbors: function(x, y) {
+      let neighborsCount = 0,
+        i,
+        j;
+
+      for (i = x - 1; i <= x + 1; ++i) {
+        if (this.isInsideTheXAxis(i, x)) {
+          for (j = y - 1; j <= y + 1; ++j) {
+            if (
+              this.isNotItself(x, y, i, j) &&
+              this.isInsideTheYAxis(j, y) &&
+              this.isNeighborAlive(i, j)
+            ) {
+              ++neighborsCount;
+            }
+          }
+        }
+      }
+
+      newRows[x][y] = this.getDestiny(neighborsCount, newRows[x][y] === LIVE);
+
+      return newRows;
+    },
+    getDestiny: function(neighborsCount, isAlive) {
+      return (isAlive && (neighborsCount === 3 || neighborsCount === 2)) ||
+        (isAlive === false && neighborsCount === 3)
+        ? LIVE
+        : DIE;
+    },
+    isNotItself: function(x, y, i, j) {
+      return (x === i && y === j) === false;
+    },
+    isInsideTheXAxis: function(index, x) {
+      return this.isInsideTheCoordinates(index, x, gridSize.x);
+    },
+    isInsideTheYAxis: function(index, y) {
+      return this.isInsideTheCoordinates(index, y, gridSize.y);
+    },
+    isInsideTheCoordinates: function(coordinate, position, gridCoordinate) {
+      return (
+        coordinate !== undefined &&
+        position !== undefined &&
+        coordinate > -1 &&
+        coordinate <= gridCoordinate
+      );
+    },
+    isNeighborAlive: function(x, y) {
+      return (
+        newRows[x] !== undefined &&
+        newRows[x][y] !== undefined &&
+        newRows[x][y] === LIVE
+      );
+    },
+    updateSurvivors: function(newRows) {
+      rows = newRows;
+
+      for (let x = 0; x < gridSize.x; x++) {
+        for (let y = 0; y < gridSize.y; y++) {
+          let thisRowStatus = rows[x][y];
+
+          canvasContext.fillStyle =
+            thisRowStatus === LIVE ? ALIVE_COLOR : DEAD_COLOR;
+
+          canvasContext.fillRect(
+            x * gridSize.z,
+            y * gridSize.z,
+            gridSize.z,
+            gridSize.z
+          );
+        }
+      }
+    },
+    selectSurvivor: function($this) {
+      var newStatus = $this.attr(DATA_STATUS_ATTRIBUTE) === DIE ? LIVE : DIE;
+
+      this.setRowStatus(
+        $this.parent().data("row"),
+        $this.data("column"),
+        newStatus
+      );
+
+      $this.toggleClass(SELECTED_CLASS_NAME);
+      $this.attr(DATA_STATUS_ATTRIBUTE, newStatus);
+      $this.css(
+        BACKGROUND_COLOR_PROPERTY,
+        colors ? this.getRandomColor() : ALIVE_COLOR
+      );
+    },
+    setRowStatus: function(x, y, status) {
+      newRows[x][y] = status;
+
+      return newRows;
+    },
+    updateSettings: function(params) {
+      if (params !== undefined) {
+        if (params.speed !== undefined) {
+          speed = params.speed;
+        }
+
+        if (params.colors !== undefined) {
+          colors = params.colors;
+        }
+      }
+    },
+    getRandomColor: function() {
+      // canvasContext.fillStyle = `
+      //   rgb(
+      //     ${Math.floor(255 - 42.5 * x)},
+      //     ${Math.floor(255 - 42.5 * y)},
+      //     0
+      //   )
+      // `;
+
+      return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    }
+  };
 })(jQuery);
