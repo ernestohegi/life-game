a6ebd5bb27df9eac5404178875a999df9ad581e6
Ernesto Hegi <ernesto.hegi@gmail.com>
2024-04-30 12:25:09 +0100

    adds vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Ernesto Hegi - JS/React version of Game of Life</title>
       </Head>
+      <Analytics />
       <Component {...pageProps} />
     </>
   );
